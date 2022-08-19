99bce553cfa2fd106523bcb8a25f01c1454b04fe
Julian Herrera Giraldo <15622477+rubberydev@users.noreply.github.com>
2022-08-19 15:53:51 -0500

    Set up project to work with Prism MVVM Dependency Injection

---
+++ TestOIS/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Prism;
+using Prism.Ioc;
+using Prism.Unity;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TestOIS
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer initializer)
+            : base(initializer)
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override async void OnInitialized()
+        {
+            InitializeComponent();
+            await NavigationService.NavigateAsync("NavigationPage/MainPage");
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+        }
+    }
+}
